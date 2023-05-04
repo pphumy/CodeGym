21339f15948172bfdc8ba1e54b7e4f6acf549c18
pphumy <keepsilentpo@gmail.com>
2023-05-04 16:29:01 +0700

    add Animal and interface Edible

--- bai_tap/Module_1/InterfaceAndAbstractClass/Animal_Edible/Program.cs
+++ bai_tap/Module_1/InterfaceAndAbstractClass/Animal_Edible/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program{
+  static void Main(string[] args)
+  {
+    Animal[] animals = new Animal[2];
+    animals[0] = new Tiger();
+    animals[1] = new Chicken();
+
+    foreach (Animal animal in animals)
+    {
+      Console.WriteLine(animal.MakeSound());
+
+      if (animal is Chicken)
+      {
+        Edible edibler = (Chicken)animal;
+        Console.WriteLine(edibler.HowToEat());
+      }
+
+    }
+    Fruit[] fruits = new Fruit[2];
+    fruits[0] = new Orange();
+    fruits[1] = new Apple();
+    foreach (Fruit fruit in fruits)
+    {
+      Console.WriteLine(fruit.HowToEat());
+    }
+}
+}
+
