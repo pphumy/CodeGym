fb117cee3e7ab859b31502b19c317abcd0de7f63
pphumy <keepsilentpo@gmail.com>
2023-05-04 17:13:50 +0700

    Add ReadTextFile

---
+++ bai_tap/Module_1/FileAndSerialization/SumOfNumInFile/Program.cs
@@ -0,0 +1,10 @@
+﻿class Program{   
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input file path");
+            string path = Console.ReadLine();
+            Console.WriteLine("File path: " + path);
+            SumOfNumInFile example = new SumOfNumInFile();
+            example.ReadTextFile(path);
+        }
+}
---
+++ bai_tap/Module_1/FileAndSerialization/SumOfNumInFile/ReadTextFile.cs
@@ -0,0 +1,29 @@
+public class SumOfNumInFile
+        {
+            public void ReadTextFile(string filePath)
+        {
+            try
+            {
+                FileInfo file = new FileInfo(filePath);
+                if (!file.Exists)
+                {
+                    throw new FileNotFoundException();
+                }
+
+                StreamReader reader = new StreamReader(filePath);
+                string line = "";
+                int sum = 0;
+                while((line = reader.ReadLine()) != null) 
+                {
+                    Console.WriteLine(line);
+                    sum += Int32.Parse(line);
+                }
+                reader.Close();
+                Console.WriteLine("Total: " + sum);
+            }
+            catch (System.Exception)
+            {
+                Console.Error.WriteLine("File not found or invalid content");
+            }
+        }
+        }
