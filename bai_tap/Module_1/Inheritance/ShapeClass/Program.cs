a421b34dcdfd559ee69645cf3cd5fe7e30fd7b2f
pphumy <keepsilentpo@gmail.com>
2023-05-04 16:40:23 +0700

    "Add Comparable"

--- bai_tap/Module_1/Inheritance/ShapeClass/Program.cs
+++ bai_tap/Module_1/Inheritance/ShapeClass/Program.cs
@@ -47,5 +47,24 @@ static void Main(string[] args)
 
                 Console.WriteLine(square);
 
+
+                ComperableCircle[] circles = new ComperableCircle[3];
+            circles[0] = new ComperableCircle(3.6);
+            circles[1] = new ComperableCircle();
+            circles[2] = new ComperableCircle(3.5, "indigo", false);
+
+            Console.WriteLine("Pre-sorted:");
+            foreach (ComperableCircle c in circles)
+            {
+                Console.WriteLine(c);
+            }
+
+            Array.Sort(circles);
+
+            Console.WriteLine("After-sorted:");
+            foreach (ComperableCircle c in circles)
+            {
+                Console.WriteLine(c);
+            }
         }
 }
